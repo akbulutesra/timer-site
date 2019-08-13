d02e86eff66a3bf053e721a7341b3beb763430b3
akbulutesra <50598326+akbulutesra@users.noreply.github.com>
2019-08-13 22:57:30 +0200

    first  commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import { FaPlus, FaMinus } from "react-icons/fa";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col></Col>
+      </Row>
+      <Row className="text-center mt-5">
+        <Col>
+          <Container>
+            <Row>
+              <Col className="titleText">
+                Break
+              </Col>
+            </Row>
+
+            <Row>
+              <Col className="numberText">
+                5
+              </Col>
+            </Row>
+
+            <Row>
+              <Col className="text-right">
+                <FaPlus />
+              </Col>
+              <Col className="text-left">
+                <FaMinus />
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+
+        <Col>
+          <Container>
+            <Row>
+              <Col className="titleText">
+                Time
+              </Col>
+            </Row>
+
+            <Row>
+              <Col className="numberText">
+                5
+              </Col>
+            </Row>
+
+            <Row>
+              <Col className="text-right">
+                <FaPlus />
+              </Col>
+              <Col className="text-left">
+                <FaMinus />
+              </Col>
+            </Row>
+          </Container>
+        </Col>
+      </Row>
+      <Row className="text-center mt-5">
+        <Col>
+          <div className="circle_container">
+            <div className="circle_main">
+              <div className="circle_text_container">
+                <div className="circle_text">
+                  25:00
+			          </div>
+              </div>
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
